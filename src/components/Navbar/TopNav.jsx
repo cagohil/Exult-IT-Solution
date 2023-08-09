2102bd2f3ce5eff1f2f1350e26f92e6c828b72b5
cagohil <chirag.gohil@exultitsolution.com>
2023-08-09 14:53:50 +0530

    about section changees donn4

--- src/components/Navbar/TopNav.jsx
+++ src/components/Navbar/TopNav.jsx
@@ -8,8 +8,8 @@ const TopNav = ({ style, rtl }) => {
           <div className="col-lg-6 col-sm-6 col-6 topBarInfo p-0">
             <div className="top-links">
               <div className="text-white d-flex">
-                <div >
-                  <a href="tel:+1 248-894-4428"><i className="fa fa-phone"></i>&nbsp; (248) 894-4428</a>
+                <div className='px-3'>
+                  <a href="tel:+1 248-894-4428"><i className="fa fa-phone"> </i>&nbsp; (248) 894-4428</a>
                   <a className='mx-3 mailHidden'>|</a>
                   <a className='mailHidden' href="mailto:info@exultitsolution.com"><i className="fa fa-envelope"></i>&nbsp; info@exultitsolution.com </a>
                 </div>
